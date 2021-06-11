4f0ef8f65b647099db5cca2de6e16d9438458b5b
Harshita Lodha <harshita.lodha@nagarro.com>
2021-06-11 17:45:20 +0530

    first commit

---
+++ src/main/java/com/nagarro/javafreshertraining/App2Application.java
@@ -0,0 +1,20 @@
+package com.nagarro.javafreshertraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dishijain
+ *
+ */
+@SpringBootApplication
+public class App2Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(App2Application.class, args);
+	}
+
+}
